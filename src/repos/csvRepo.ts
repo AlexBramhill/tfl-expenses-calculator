8452c8b09b22f54bebfcf2fb640eba5b33e78378
Alex Bramhill <alex.bramhill@softwire.com>
2026-05-19 10:45:23 +0100

    update csv folder creation

--- src/repos/csvRepo.ts
+++ src/repos/csvRepo.ts
@@ -1,14 +1,25 @@
 import fs from "node:fs/promises";
 import path from "node:path";
-import { logDebug } from "../logPublisher";
+import {logDebug} from "../logPublisher";
 
 export const listCsvFiles = async (folder: string) => {
-	logDebug(`Listing CSV files in folder: ${folder}`);
-	const files = await fs.readdir(folder);
-	logDebug(`Files ${files.length} files in folder: ${folder}`);
-	const csvs = files
-		.filter((file) => file.endsWith(".csv"))
-		.map((file) => path.join(folder, file));
-	logDebug(`Found ${csvs.length} CSV files in folder: ${folder}`);
-	return csvs;
+    logDebug(`Listing CSV files in folder: ${folder}`);
+    try {
+        const files = await fs.readdir(folder);
+        logDebug(`Files ${files.length} files in folder: ${folder}`);
+
+        const csvs = files
+            .filter((file) => file.endsWith(".csv"))
+            .map((file) => path.join(folder, file));
+        logDebug(`Found ${csvs.length} CSV files in folder: ${folder}`);
+        return csvs;
+    } catch (err) {
+        if (
+            err instanceof Error &&
+            (err as NodeJS.ErrnoException).code === "ENOENT"
+        ) {
+            fs.mkdir(folder, { recursive: true });
+            return []
+        }
+    }
 };
