d66d131c576de84aa75050e39f7827424d3647cc
Alex Bramhill <alexbramhill@gmail.com>
2026-07-29 22:37:45 +0100

    extract journey group row

--- src/components/JourneyExplorer.tsx
+++ src/components/JourneyExplorer.tsx
@@ -1,49 +1,52 @@
-import { Box, Text } from "ink";
-import useGroupedJourneys from "../hooks/useGroupedJourneys";
+import {Box, Text} from "ink";
+import useGroupedJourneys, {type JourneyGroup} from "../hooks/useGroupedJourneys";
 import useJourneys from "../hooks/useJourneys";
 import useListNavigation from "../hooks/useListNavigation";
-import type { Config } from "../repos/configRepo";
+import type {Config} from "../repos/configRepo";
 
-export const JourneyExplorer = ({ config }: { config: Config }) => {
-	const unsortedJourneys = useJourneys(config.csvFolder);
-	const {groupedJourneys, journeyGrouping, setJourneyGrouping} = useGroupedJourneys(unsortedJourneys);
-	const { currentItem } = useListNavigation(
-		groupedJourneys,
-		(item) => item.displayName,
-	);
+const journeyGroupRow = (item: JourneyGroup) => {
+    const isSelected = item?.displayName === item.displayName
+    return (
+        <Text
+            key={item.displayName}
+            color={
+                isSelected
+                    ? "green"
+                    : undefined
+            }
+            dimColor={!isSelected}
+        >
+            {isSelected ? ">" : " "}{item.displayName}
+        </Text>
+    );
+}
 
-	if (unsortedJourneys.status === "loading") return <Text>Loading...</Text>;
-	if (unsortedJourneys.status === "error")
-		return <Text>Error: {unsortedJourneys.error.message}</Text>;
-	if (groupedJourneys.length === 0) return <Text>No journeys found.</Text>;
+export const JourneyExplorer = ({config}: { config: Config }) => {
+    const unsortedJourneys = useJourneys(config.csvFolder);
+    const {groupedJourneys, journeyGrouping, setJourneyGrouping} = useGroupedJourneys(unsortedJourneys);
+    const {currentItem} = useListNavigation(
+        groupedJourneys,
+        (item) => item.displayName,
+    );
 
-	return (
-		<Box flexDirection="row" gap={2} flexGrow={1}>
-			<Box flexDirection="column">
-				{groupedJourneys.map((item) => {
-					return (
-						<Text
-							key={item.displayName}
-							color={
-								currentItem?.displayName === item.displayName
-									? "green"
-									: undefined
-							}
-							dimColor={currentItem?.displayName !== item.displayName}
-						>
-							{item.displayName}
-						</Text>
-					);
-				})}
-			</Box>
-			{/*{currentSelectedFile &&*/}
-			{/*    selectedResult &&*/}
-			{/*    !(selectedResult instanceof Error) && (*/}
-			{/*        <FileDetailPanels*/}
-			{/*            key={currentSelectedFile}*/}
-			{/*            journeysResult={selectedResult}*/}
-			{/*        />*/}
-			{/*    )}*/}
-		</Box>
-	);
+    if (unsortedJourneys.status === "loading") return <Text>Loading...</Text>;
+    if (unsortedJourneys.status === "error")
+        return <Text>Error: {unsortedJourneys.error.message}</Text>;
+    if (groupedJourneys.length === 0) return <Text>No journeys found.</Text>;
+
+    return (
+        <Box flexDirection="row" gap={2} flexGrow={1}>
+            <Box flexDirection="column">
+                {groupedJourneys.map(journeyGroupRow)}
+            </Box>
+            {/*{currentSelectedFile &&*/}
+            {/*    selectedResult &&*/}
+            {/*    !(selectedResult instanceof Error) && (*/}
+            {/*        <FileDetailPanels*/}
+            {/*            key={currentSelectedFile}*/}
+            {/*            journeysResult={selectedResult}*/}
+            {/*        />*/}
+            {/*    )}*/}
+        </Box>
+    );
 };
